5c446b472764806a805b57142db37d43bf6583db
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-06-21 07:58:50 -0700

    remove doctests

--- lib/shout_box/messages/messages.ex
+++ lib/shout_box/messages/messages.ex
@@ -8,51 +8,16 @@ defmodule ShoutBox.Messages do
 
   alias ShoutBox.Messages.Shout
 
-  @doc """
-  Returns the list of shouts.
-
-  ## Examples
-
-      iex> list_shouts()
-      [%Shout{}, ...]
-
-  """
   def list_shouts do
     Repo.all(from s in Shout, preload: :user)
   end
 
-  @doc """
-  Gets a single shout.
-
-  Raises `Ecto.NoResultsError` if the Shout does not exist.
-
-  ## Examples
-
-      iex> get_shout!(123)
-      %Shout{}
-
-      iex> get_shout!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_shout!(id) do
     Shout
     |> Repo.get!(id) 
     |> Repo.preload(:user)
   end
 
-  @doc """
-  Creates a shout.
-
-  ## Examples
-
-      iex> create_shout(%{field: value})
-      {:ok, %Shout{}}
-
-      iex> create_shout(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_shout(message, user) do
     user
     |> Ecto.build_assoc(:shouts)
@@ -60,15 +25,6 @@ defmodule ShoutBox.Messages do
     |> Repo.insert()
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking shout changes.
-
-  ## Examples
-
-      iex> change_shout(shout)
-      %Ecto.Changeset{source: %Shout{}}
-
-  """
   def change_shout(%Shout{} = shout) do
     Shout.changeset(shout, %{})
   end
--- test/shout_box/messages/messages_test.exs
+++ test/shout_box/messages/messages_test.exs
@@ -2,8 +2,4 @@ defmodule ShoutBox.MessagesTest do
   use ShoutBox.DataCase
 
   alias ShoutBox.Messages
-
-  describe "shouts" do
-    alias ShoutBox.Messages.Shout
-  end
 end
